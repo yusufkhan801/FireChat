f3d20267272ea28d9ceb6bad152b9495439e7d43
yusufkhan801 <61074508+yusufkhan801@users.noreply.github.com>
2021-09-13 13:15:32 +0100

    Build-v1.1
    --nothing too far

---
+++ firebase/SDK.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDR7M8GHEcdtyDmPKhHb-PaIBD0eOzsy5c",
+    authDomain: "firechat-v1-331a4.firebaseapp.com",
+    projectId: "firechat-v1-331a4",
+    storageBucket: "firechat-v1-331a4.appspot.com",
+    messagingSenderId: "85568350572",
+    appId: "1:85568350572:web:e0026cf75e6592ea8aeb49"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
